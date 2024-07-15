9fa5a7dafbc6ce6143602acb0eec02d2ffb6567f
ngkwdsk <nagakawadai@gmail.com>
2024-07-15 17:19:19 +0900

    indexページへのデータ表示

--- db/migrate/20240714075751_create_podcasts.rb
+++ db/migrate/20240714075751_create_podcasts.rb
@@ -1,11 +1,8 @@
 class CreatePodcasts < ActiveRecord::Migration[7.0]
   def change
     create_table :podcasts do |t|
-      t.string :name,      null: false
-      t.text   :discription, null: false
+      t.string :show_id,      null: false
       t.timestamps
-
-      has_one_attached :image
     end
   end
 end
