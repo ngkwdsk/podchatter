d18a3efb78e6c9c79850955d33e6ddde77429922
ngkwdsk <nagakawadai@gmail.com>
2024-07-21 12:29:23 +0900

    番組データの保存

--- app/controllers/podcasts_controller.rb
+++ app/controllers/podcasts_controller.rb
@@ -1,39 +1,47 @@
 class PodcastsController < ApplicationController
-  before_action :set_post, only: [:edit]
-  before_action :move_to_index, except: [:index, :show]
+  before_action :set_podcast, only: [:show]
+  before_action :move_to_index, except: [:index, :show, :search]
   
   require 'rspotify'
   RSpotify.authenticate(ENV['SPOTIFY_CLIENT_ID'], ENV['SPOTIFY_CLIENT_SECRET'])
   
-  def search
-    @podcast = Podcast.new
-    if params[:search].present?
-      @searchpodcasts = RSpotify::Artist.search(params[:search])
-      Rails.logger.debug @searchpodcasts.inspect
-    end
-  end
-
-  def show 
-  end
-
   def index
     @podcasts = Podcast.all
     @podcast_data = @podcasts.map{ |podcast| get_podcast_data(podcast) }
   end
   
-  private
-
-  PodcastData = Struct.new(:image, :name, :description)
-
-  def get_podcast_data(spotify)
-    show = RSpotify::Show.find(spotify.show_id)
-    return if show.nil?
-    image = show.images.empty? ? nil : show.images.first["url"]
-    PodcastData.new(image, show.name, show.description)
+  def show
   end
 
-  def set_post
-    @podcast =Podcast.find(params[:id])
+  def search
+    @podcast = Podcast.new
+    if params[:search].present?
+      @searchpodcasts = RSpotify::Show.search(params[:search], limit: 10, market: 'JP')
+    end
+  end
+
+  def create
+    @podcast = Podcast.new(show_id: params[:show_id])
+    if @podcast.save
+      redirect_to podcasts_path, notice: '登録されました！'
+    else
+      redirect_to search_podcasts_path, alert: @podcast.errors.full_messages.to_sentence
+    end
+  end
+
+  private
+
+  PodcastData = Struct.new(:id, :image, :name, :description)
+
+  def get_podcast_data(spotify_show)
+    show = RSpotify::Show.find(spotify_show.show_id)
+    return if show.nil?
+    image = show.images.empty? ? nil : show.images.first["url"]
+    PodcastData.new(spotify_show.id, image, show.name, show.description)
+  end
+
+  def set_podcast
+    @podcast = get_podcast_data(Podcast.find(params[:id]))
   end
   
   def move_to_index
@@ -41,6 +49,11 @@ class PodcastsController < ApplicationController
       redirect_to root_path
     end
   end
+
+  def podcast_params
+    params.require(:podcast).permit(:show_id)
+  end
+  
 end
 
 	
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :podcasts do
-    collection {get "search"}
+    collection do
+    get :search
+    post :create_from_search
+    end
   end
   root to: "podcasts#index"
 end
